852810e3e0eedc68add6271652d7cbc79f67aac9
Dillon Thornberry <dthornberry94@gmail.com>
2020-11-20 02:53:14 -0700

    added options static

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser())
 app.use(cors({ origin: 'https://robot-ape.herokuapp.com/options', credentials: true }))
 
 app.use(express.static(path.join(__dirname, 'homepage-build')))
-// app.use(express.static(path.join(__dirname, 'options-build')))
+app.use(express.static(path.join(__dirname, 'options-build')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'homepage-build', 'index.html'));
